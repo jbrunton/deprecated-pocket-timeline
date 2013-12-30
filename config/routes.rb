be3d10647af481ba14b13e78d7b5b71d128ff50a
John Brunton <john_brunton@hotmail.co.uk>
2013-12-30 22:47:06 +0000

    rails scaffold for events

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 PocketTimeline::Application.routes.draw do
+  resources :events
+
   root 'home#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ spec/views/events/show.html.erb_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe "events/show" do
+  before(:each) do
+    @event = assign(:event, stub_model(Event,
+      :title => "Title",
+      :description => "MyText"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    expect(rendered).to match(/Title/)
+    expect(rendered).to match(/MyText/)
+  end
+end
