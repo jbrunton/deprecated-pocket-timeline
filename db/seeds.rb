f8934d946e0b03f3907814f1f826ce7d637e45bb
John Brunton <john_brunton@hotmail.co.uk>
2014-01-04 18:11:03 +0000

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-iranian_revolution = Event.create(title: 'Islamic Revolution of Iran', description: 'Overthrow of the Shah')
-iranian_revolution_timeline = Timeline.create(title: 'Islamic Revolution of Iran', description: 'Overthrow of the Shah')
-iranian_revolution_timeline.events.push iranian_revolution
+Timeline.create(
+  title: 'World War II',
+  description: 'Events of the Second World War',
+  events: [
+    Event.create(
+      title: 'Germany invades Poland',
+      date: DateTime.new(1939, 9, 1)
+    ),
+    Event.create(
+      title: 'Britain goes to war',
+      date: DateTime.new(1939, 9, 3)
+    ),
+    Event.create(
+      title: 'Pearl Harbor',
+      date: DateTime.new(1941, 12, 7)
+    ),
+    Event.create(
+      title: 'Germany surrenders',
+      description: 'Germany signs the German Instrument of Surrender',
+      date: DateTime.new(1945, 5, 7)
+    ),
+    Event.create(
+      title: 'Japan surrenders',
+      date: DateTime.new(1945, 9, 2)
+    )
+  ]
+)
